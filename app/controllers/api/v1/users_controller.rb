ac29619ec1ba449069d3b9e5e8d8f69c35495fef
ReginaF2012 <skritschy@gmail.com>
2021-01-17 23:55:38 -0600

    user sign up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -26,6 +26,12 @@ class Api::V1::UsersController < ApplicationController
       
   def create
     @user = User.new(user_params)
+    if @user
+      token = JsonWebToken.encode({user_id: @user.id})
+      render json: @user, {token: token, status: 200}
+    else
+      render json: {errors: 'invalid username, email, or password', status: :unauthorized}
+    end
   end
 
   private
