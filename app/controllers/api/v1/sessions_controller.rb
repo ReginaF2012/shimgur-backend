e6fbeeb1c6edd862a1aa89695cead22a5201b817
ReginaF2012 <skritschy@gmail.com>
2021-01-17 23:51:26 -0600

    login and autologin functionality complete

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::SessionsController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def create
+        if params[:email]
+            @user = User.find_by(params[:email])
+        else
+            @user = User.find_by(params[:username])
+        end
+
+        if @user && @user.authenticate(params[:password])
+            payload = {user_id: @user.id}
+            token = JsonWebToken.encode(payload)
+            render json: {@user, token: token}
+        else
+            render json: {errors: 'invalid credentials'}, status: :unauthorized
+        end
+    end
+
+    def autologin
+        token = JsonWebToken.encode({user_id: current_user.id})
+        render json: {user: current_user, token: token}
+    end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API   
+    before_action :authorized
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JsonWebToken.decode(token)
+            rescue 
+                nil
+            end
+        end
+    end
+
+    def current_user
+        if decoded_token
+          user_id = decoded_token['user_id']
+          user = User.find_by(id: user_id)
+        end
+    end
+        
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        render json: {errors: 'Please log in' }, status: :unauthorized unless logged_in?
+    end
 end
